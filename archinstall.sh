42acd0873d4f1902254e4d0e8c8594994b20ad14
Alvaro Oliveira <51033564+limaon@users.noreply.github.com>
2024-10-25 08:33:25 -0400

    Refactor archinstall.sh to improve logging and script organization.

--- archinstall.sh
+++ archinstall.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
 #github-action genshdoc
 #
-# @file ArchInstaller
+# @file archinstall.sh
 # @brief Entrance script that launches children scripts for each phase of installation.
+# @stdout Output routed to install.log
+# @stderror Output routed to install.log
+# shellcheck disable=SC1090,SC1091
 
 # Find the name of the folder the scripts are in
 set -a
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts
-CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/configs
-set +a
-echo -ne "
--------------------------------------------------------------------------
-                    Automated Arch Linux Installer
--------------------------------------------------------------------------
-                Scripts are in directory named ArchInstaller
-"
-    ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
-      source $CONFIGS_DIR/setup.conf
-    ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-    ( arch-chroot /mnt/sysArch $HOME/ArchInstaller/scripts/1-setup.sh )|& tee 1-setup.log
-    if [[ ! $DESKTOP_ENV == server ]]; then
-      ( arch-chroot /mnt/sysArch /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchInstaller/scripts/2-user.sh )|& tee 2-user.log
-    fi
-    ( arch-chroot /mnt/sysArch $HOME/ArchInstaller/scripts/3-post-setup.sh )|& tee 3-post-setup.log
-    cp -v *.log /mnt/sysArch/home/$USERNAME
 
-echo -ne "
--------------------------------------------------------------------------
-                    Automated Arch Linux Installer
--------------------------------------------------------------------------
-                Done - Please Eject Install Media and Reboot
-"
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPTS_DIR="${SCRIPTS_DIR}/scripts"
+CONFIGS_DIR="${SCRIPTS_DIR}/configs"
+
+BOLD='\e[1m'
+RESET='\e[0m'
+BRED='\e[91m]'
+
+set +a
+
+
+# Delete existing log file and log output of script
+[[ -f "$LOG_FILE" ]] && rm -f "$LOG_FILE"
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+
+# Load utility scripts
+for filename in "$SCRIPTS_DIR"/utils/*.sh; do
+  [ -e "$filename" ] || continue
+  # shellcheck source=$SCRIPTS_DIR/utils/*.sh
+  source "$filename"
+done
+
+# echo -ne "
+# -------------------------------------------------------------------------
+#                     Automated Arch Linux Installer
+# -------------------------------------------------------------------------
+#                 Scripts are in directory named ArchInstaller
+# "
+#     ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
+#       source $CONFIGS_DIR/setup.conf
+#     ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
+#     ( arch-chroot /mnt/sysArch $HOME/ArchInstaller/scripts/1-setup.sh )|& tee 1-setup.log
+#     if [[ ! $DESKTOP_ENV == server ]]; then
+#       ( arch-chroot /mnt/sysArch /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchInstaller/scripts/2-user.sh )|& tee 2-user.log
+#     fi
+#     ( arch-chroot /mnt/sysArch $HOME/ArchInstaller/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+#     cp -v *.log /mnt/sysArch/home/$USERNAME
+#
+# echo -ne "
+# -------------------------------------------------------------------------
+#                     Automated Arch Linux Installer
+# -------------------------------------------------------------------------
+#                 Done - Please Eject Install Media and Reboot
+# "
