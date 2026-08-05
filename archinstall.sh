689dead5f929e6ef4c3f05fc67e807baa53125b4
Alvaro Oliveira <51033564+limaon@users.noreply.github.com>
2026-08-05 11:12:30 -0400

    Revert "chore(scripts): use /usr/bin/env bash shebang for portability"
    This reverts commit 14c635d6fe03b73eab4cfa09f79baec72b5cbf08.

--- archinstall.sh
+++ archinstall.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #github-action genshdoc
 #
 # @file archinstall.sh
--- scripts/utils/logging.sh
+++ scripts/utils/logging.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 ##############################################################################
 # ArchInstaller Logging Module (Simplified)
